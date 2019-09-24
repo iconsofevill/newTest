3ecb0c3c1fd13210c7dcfb2794efe7a86440f0ad
iconsofevill <47002949+iconsofevill@users.noreply.github.com>
2019-09-24 21:00:04 +0300

    new methods

--- src/CalcInterface.php
+++ src/CalcInterface.php
@@ -4,7 +4,34 @@ namespace App;
 
 interface CalcInterface {
 
+    // добавь!
     public function plus (float $a, float $b) : float;
+
+    // отними!!
     public function minus (float $a, float $b) : float;
 
+    // умножь!!!
+    public function mult (float $a, float $b) : float;
+
+    // раздели!!!!
+    public function div (float $a, float $b) : float;
+
+    // логорифни!!!!!
+    public function log (float $a, float $b) : float;
+
+    // натурально логорифни!!!!!!
+    public function ln (float $a) : float;
+
+    // возведи в квадрат!!!!!!!
+    public function pow (float $a, float $b) : float;
+
+    // процент сделай срочно мне!!!!!!!!
+    public function per (float $a, float $b) : float;
+
+    // корень срочнee!!!!!!!!!!
+    public function sqrt (float $a) : float;
+
+    // факториал отсрачивать вообще нельзя!!!!!!!!!!!
+    public function fact (float $a) : float;
+    
 }
